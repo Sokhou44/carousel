94d67f154b7aebcd6b573111db7ff3591a898411
Sokhna Gueye <sokhnag98@gmail.com>
2025-05-21 14:27:05 +0200

    ajout du script

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+
+"use strict"
+
 const button = document.getElementById ("btn");
-const carousel = document.getElementById ("carousel");
 
-console.log(button)
+const images = document.querySelectorAll(".carousel img");
 
+let index = 0;
+
+function afficherImage(i) {
+    for (let j = 0; j < images.length; j++) {
+        images[j].classList.remove("actif");
+}
+
+  images[i].classList.add("actif");
+
+}
+
+  afficherImage(index);
+
+  button.addEventListener("click", function() {
+    index = index + 1;
+  
+  if (index >= images.length) {
+    index = 0;
+  } 
+
+  afficherImage(index);
+});
